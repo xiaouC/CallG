43ee44c2818db35fd5d9bbaeb64a74ce521f4e41
xiaou <yyolq0410@163.com>
2015-11-20 18:42:11 +0800

    fix bug

--- src/com/yy2039/callguardian/YYShowAlertDialog.java
+++ src/com/yy2039/callguardian/YYShowAlertDialog.java
@@ -97,7 +97,7 @@ public class YYShowAlertDialog {
             });
         }
 
-        //cur_show_ad.setCanceledOnTouchOutside( true );   // 设置点击 Dialog 外部任意区域关闭 Dialog
+        cur_show_ad.setCanceledOnTouchOutside( false );   // 设置点击 Dialog 外部任意区域关闭 Dialog
         cur_show_ad.show();
 
         return cur_show_ad;
@@ -254,7 +254,7 @@ public class YYShowAlertDialog {
             });
         }
 
-        ad.setCanceledOnTouchOutside( true );   // 设置点击 Dialog 外部任意区域关闭 Dialog
+        ad.setCanceledOnTouchOutside( false );   // 设置点击 Dialog 外部任意区域关闭 Dialog
         ad.show();
     }
 
