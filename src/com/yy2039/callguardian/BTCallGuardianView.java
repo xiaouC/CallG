f1c63c32d4cccec581611d5a6513be66e25412a7
xiaou <yyolq0410@163.com>
2015-11-20 13:35:31 +0800

    fix bug & v1.5

--- src/com/yy2039/callguardian/BTCallGuardianView.java
+++ src/com/yy2039/callguardian/BTCallGuardianView.java
@@ -943,7 +943,7 @@ public class BTCallGuardianView extends YYViewBack {
                                 btn_obj.setText( YYViewBase.transferText( area_code, "" ) );
                                 btn_obj.setOnClickListener( new View.OnClickListener() {
                                     public void onClick( View v ) {
-                                        main_activity.yy_data_source.onMedaProcess( YYCommand.ADD_NEW_AREA_CODE, add_area_code, area_code, new YYDataSource.onMedaListener() {
+                                        main_activity.yy_data_source.onMedaProcess( YYCommand.EDIT_ONE_AREA_CODE, add_area_code, area_code, new YYDataSource.onMedaListener() {
                                             public void onSuccessfully() {
                                                 YYViewBase.onBackClick();
 
@@ -957,7 +957,7 @@ public class BTCallGuardianView extends YYViewBack {
                                                 });
                                             }
                                             public void onFailure( int err_code ) {
-                                                Toast.makeText( main_activity, "add area code failed", Toast.LENGTH_LONG ).show();
+                                                Toast.makeText( main_activity, "edit area code failed", Toast.LENGTH_LONG ).show();
                                             }
                                         });
                                     }
