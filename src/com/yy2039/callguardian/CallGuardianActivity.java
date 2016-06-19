8777c3f6e777a97843ebf278e8479cd081d0acfb
xiaou <yyolq0410@163.com>
2016-06-19 12:06:29 +0800

    fix incoming call

--- src/com/yy2039/callguardian/CallGuardianActivity.java
+++ src/com/yy2039/callguardian/CallGuardianActivity.java
@@ -133,13 +133,7 @@ public class CallGuardianActivity extends FragmentActivity
     private BroadcastReceiver incomingCallReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            if( yy_playing_msg_dlg != null ) {
-                Intent intent1 = new Intent();  
-                intent1.setAction( ANSWER_MACHINE_CHANGE_NORMAL );
-                sendBroadcast( intent1 );
-
-                yy_playing_msg_dlg = null;
-            }
+            yy_playing_msg_dlg = null;
 
             finish();
         }
